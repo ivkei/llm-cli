40f22eb655ca710b66689da2a9b42705624395fb
debugkei <ivankei0009@gmail.com>
2025-03-10 19:38:37 -0700

    Prepared format for llms support

--- src/cl_parser.py
+++ src/cl_parser.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 
 # Create arg parser
 parser = argparse.ArgumentParser("Argument parser for llm_cli.py.")
@@ -7,9 +8,11 @@ parser = argparse.ArgumentParser("Argument parser for llm_cli.py.")
 parser.add_argument("url", help="The LLM server url.", type=str)
 parser.add_argument("model", help="The name of the model that is going to be used from the server.", type=str)
 
-parser.add_argument("-d", "--dir", help="The directory for LLM to read.")
-parser.add_argument("-f", "--file", help="The file for llm to read.")
-parser.add_argument("-t", "--temp", help="The temperature of LLM's response.")
+parser.add_argument("-a", "--api", help="The API for a model, only specify if not running locally.", type=str)
+
+parser.add_argument("-d", "--dir", help="The directory for LLM to read.", type=pathlib.Path)
+parser.add_argument("-f", "--file", help="The file for llm to read.", type=argparse.FileType('r'))
+parser.add_argument("-t", "--temp", type=int, help="The temperature of LLM's response. If not specified then 0.7 is used. The closer to 0 the more straight-forward the output is.")
 
 parser.add_argument("prompt", help="User's prompt", nargs='*')
 
--- src/llm_cli.py
+++ src/llm_cli.py
@@ -5,29 +5,50 @@ def main():
   args = cl_parser.GetArgs()
 
   # Create llm server access instance
-  llm = OpenAI(base_url=args.url, api_key="not-needed")
+  llm = OpenAI(base_url=args.url, api_key=args.api or "not-needed")
+
+  # Contents is additional file contents
+  contents = ""
+
+  # Requirements is how I ask LLM to format output
+  requirements = """"""
 
   # File or directory
   if "dir" in args.__dict__:
     pass
   elif "file" in args.__dict__:
     pass
-
-  stream = llm.chat.completions.create(model=args.model, stream=True,
-    messages=[{"role": "user", "content": "".join(args.prompt)}],
+  
+  # Generate a stream output
+  stream = llm.chat.completions.create(
+    model=args.model,
+    stream=True,
+    temperature=args.temp or 0.7,
+    messages=[
+              { "role":
+                  "user",
+                "content":
+                  "<<Requirements>>" + requirements + "<<!Requirements>>" +
+                  "<<Contents>>" + contents + "<<!Contents>>" +
+                  "<<Client's prompt>>" + "".join(args.prompt) + "<<!Client's prompt>>"
+              }
+             ],
     )
 
+  # Output the answer
   for chunk in stream:
     print(chunk.choices[0].delta.content or '', end='', flush=True)
   print()
 
+# Call main
 if __name__ == "__main__":
-  try:
+  # try:
     main()
-  except Exception as e:
-    print("\033[31m", "Error: ", "{ ", e, " }", "\033[0m", sep='')
+  # except Exception as e:
+  #   print("\033[31m", "Error: ", "{ ", e, " }", "\033[0m", sep='')
 
 # TODO:
-# Add API key argv
 # Argv parameters docs
-# Render in markdown??
+# Interruption support
+# File or Dir fetching contents support
+# Youtube video link fetch transcript and analyze it
